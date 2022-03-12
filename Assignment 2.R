6023c5657b1485404d472b68735ffb2e22fc519c
austinessmann <60491371+austinessmann@users.noreply.github.com>
2022-03-11 23:53:56 -0500

    Add files via upload

---
+++ Assignment 2.R
@@ -0,0 +1,22 @@
+library (tidyverse)
+library(readr)
+#read in data
+US<-read_csv("us-states.csv")
+PA<-filter(US, state=="Pennsylvania")
+n<-length(US$date)
+
+#increase cases variable
+PA$incr_cases<-1
+for (i in 2:n)
+{
+  PA$incr_cases[i]<-(PA$cases[i]-PA$cases[i-1])
+}
+#increase in deaths variable
+PA$incr_deaths<-1
+for (i in 2:n)
+{
+  PA$incr_deaths[i]<-(PA$deaths[i]-PA$deaths[i-1])
+}
+#check work
+check<-sd(PA$incr_cases)
+check
